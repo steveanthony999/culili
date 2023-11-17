a9be80bb18fdbf8d2e873922aadd387833140cf2
Steven Woodward <steveanthony999@gmail.com>
2023-11-17 09:30:59 -0800

    Working on user project creation, testing for production

--- controllers/projectsController.js
+++ controllers/projectsController.js
@@ -1,4 +1,6 @@
 const asyncHandler = require('express-async-handler');
+const fs = require('fs').promises;
+const path = require('path');
 
 const User = require('../models/userModel');
 const Project = require('../models/projectModel');
@@ -17,7 +19,6 @@ const createProject = asyncHandler(async (req, res) => {
     throw new Error('Please add a Project Name and description');
   }
 
-  // Get user using the id in the JWT
   const user = await User.findById(req.user.id);
 
   if (!user) {
@@ -26,16 +27,30 @@ const createProject = asyncHandler(async (req, res) => {
     throw new Error('User not found');
   }
 
+  const userProjectsBaseDir = path.join(__dirname, '..', 'user_projects');
+  const projectDirectory = path.join(userProjectsBaseDir, projectName);
+  try {
+    await fs.mkdir(projectDirectory, { recursive: true });
+  } catch (error) {
+    console.error('Error creating project directory:', error);
+    res.status(500);
+    throw new Error('Server error occurred while creating project space');
+  }
+
   const project = await Project.create({
     projectName,
     description,
     userId: req.user.id,
+    directory: projectDirectory
   });
 
   user.numProjects += 1;
   await user.save();
 
-  res.status(201).json(project);
+  res.status(201).json({
+    project,
+    message: `Project created successfully. Access your project at ${projectDirectory}`
+  });
 });
 
 module.exports = {
