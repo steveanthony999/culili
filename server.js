72230a37868e2cd1dbe91fa44cd353df52287915
Steven Woodward <steveanthony999@gmail.com>
2023-11-20 23:30:37 -0800

    Added path to server

--- server.js
+++ server.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const cors = require('cors');
+const path = require('path');
 const dotenv = require('dotenv');
 const routes = require('./routes/index');
 const userRoutes = require('./routes/userRoutes');
